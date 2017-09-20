085cd434103d6cc66cebd63ab14c27e7973d4f2b
dkaznacheev <dmitry.kaznacheev@gmail.com>
2017-09-20 22:13:52 +0300

    Added Javadoc comments

--- src/ru/spbau/dkaznacheev/HashTable.java
+++ src/ru/spbau/dkaznacheev/HashTable.java
@@ -1,10 +1,26 @@
 package ru.spbau.dkaznacheev;
 
+/**
+* Stores elements of java.lang.String inside by their hash codes.
+* Automatically resizes if the size exceeds a certain fraction of capacity.
+ **/
 public class HashTable {
-
+    /**
+     * Current number of elements in the table.
+     **/
     private int size;
+    /**
+     * Current capacity of the table.
+     **/
     private int capacity;
+    /**
+     * The storage, consists of Lists, which contain the elements.
+     **/
     private List storage[];
+
+    /**
+     * The size-to-capacity ratio after which the resizing happens.
+     **/
     private final double SIZE_PERCENT_LIMIT = 0.75;
 
     public HashTable () {
@@ -19,6 +35,11 @@ public class HashTable {
         size = 0;
     }
 
+    /**
+     * Returns the hash code of the given string.
+     * @param string The string that gets hashed.
+     * @return The hash.
+     **/
     private int getHash(String string) {
         int stringHash = 7;
         for (int i = 0; i < string.length(); i++) {
@@ -27,11 +48,20 @@ public class HashTable {
         return stringHash;
     }
 
+    /**
+     * Finds the position in the storage array by the key.
+     * @param key The given key
+     * @return The index in the array.
+     **/
     public int getIndex (String key) {
         int hash = getHash(key);
         return hash & (capacity - 1);
     }
 
+    /**
+     * Resizes the table.
+     * @param newCapacity New capacity of the table.
+     **/
     private void resize (int newCapacity) {
         HashTable newTable = new HashTable(newCapacity);
         for (int i = 0; i < capacity; i++) {
@@ -44,15 +74,30 @@ public class HashTable {
         this.capacity = newTable.capacity;
     }
 
+    /**
+     * Returns how many elements is in the table.
+     * @return The table size.
+     **/
     public int size () {
         return size;
     }
 
+    /**
+     * Checks if there is an element with the given key in the table.
+     * @param key The given key.
+     * @return Whether there is an element.
+     **/
     public boolean contains (String key) {
         int index = getIndex(key);
         return storage[index].contains(key);
     }
 
+    /**
+     * Changes or adds the value of the given key, returns the previous value.
+     * @param key The key
+     * @param value The new value
+     * @return Previous value, null if there was no value with this key.
+     **/
     public String put (String key, String value) {
         int index = getIndex(key);
         if (!storage[index].contains(key))
@@ -63,18 +108,29 @@ public class HashTable {
         return result;
     }
 
+    /**
+     * Returns the value stored with given key.
+     * @param key The key.
+     * @return The value stored, null if there was no value with this key.
+     **/
     public String get (String key) {
         int index = getIndex(key);
         return storage[index].get(key);
     }
-
+    /**
+     * Returns the value stored with given key and removes the key-value pair from the table.
+     * @param key The key.
+     * @return The value stored, null if there was no value with this key.
+     **/
     public String remove (String key) {
         int index = getIndex(key);
         if (!storage[index].contains(key))
             size--;
         return storage[index].remove(key);
     }
-
+    /**
+     * Clears the table.
+     **/
     public void clear() {
         for (int i = 0; i < capacity; i++)
             storage[i] = new List();
--- src/ru/spbau/dkaznacheev/List.java
+++ src/ru/spbau/dkaznacheev/List.java
@@ -1,18 +1,30 @@
 package ru.spbau.dkaznacheev;
-
+/**
+ * Linked list that stores elements of java.lang.String.
+ **/
 public class List {
-
+    /**
+     * Node Subclass that describes the key-value pair.
+     **/
     class Node {
         String key;
         String value;
+        /**
+         * The next node.
+         **/
         Node next;
         Node (String key, String value) {
             this.key = key;
             this.value = value;
         }
     }
-
+    /**
+     * The first element of the list.
+     **/
     private Node head;
+    /**
+     * Size of the list.
+     **/
     private int size;
 
     public List () {
@@ -29,7 +41,11 @@ public class List {
     public int size() {
         return size;
     }
-
+    /**
+     * Returns the value of the node with given key.
+     * @param key The key.
+     * @return The value, null if there is no such key.
+     **/
     public String get (String key) {
         if (head == null)
             return null;
@@ -41,7 +57,12 @@ public class List {
         } while (node != null);
         return null;
     }
-
+    /**
+     * Changes or adds the value of the given key, returns the previous value.
+     * @param key The key
+     * @param value The new value
+     * @return Previous value, null if there was no value with this key.
+     **/
     public String put (String key, String value) {
         if (head == null) {
             head = new Node(key, value);
@@ -63,7 +84,10 @@ public class List {
         size++;
         return null;
     }
-
+    /**
+     * Returns the first key-value pair of the list, and then deletes it.
+     * @return First node, or null if the list is empty.
+     **/
     public Node popFront () {
         if (head == null)
             return null;
@@ -72,7 +96,11 @@ public class List {
         size--;
         return result;
     }
-
+    /**
+     * Returns the value stored with given key and removes the key-value pair from the list.
+     * @param key The key.
+     * @return The value stored, null if there was no value with this key.
+     **/
     public String remove (String key) {
         if (head == null)
             return null;
@@ -94,7 +122,11 @@ public class List {
         } while (node != null);
         return null;
     }
-
+    /**
+     * Checks if there is an element with the given key in the list.
+     * @param key The given key.
+     * @return Whether there is an element.
+     **/
     public boolean contains (String key) {
         return (get(key) != null);
     }
