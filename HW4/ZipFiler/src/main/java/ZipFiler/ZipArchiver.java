eeee60434a3187ba051cf8071597f22593e0aa94
dkaznacheev <dmitry.kaznacheev@gmail.com>
2017-12-04 17:18:11 +0300

    Fixed and cleaned HW4

--- HW4/ZipFiler/src/main/java/ZipFiler/ZipArchiver.java
+++ HW4/ZipFiler/src/main/java/ZipFiler/ZipArchiver.java
@@ -1,15 +1,16 @@
+package ZipFiler;
+
 import java.io.*;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
 /**
- * ZipArchiver is a class that can unzip files in directory that match the given regex.
+ * ZipFiler.ZipArchiver is a class that can unzip files in directory that match the given regex.
  */
 public class ZipArchiver {
-
-
     /**
      * Path to file
      */
@@ -23,14 +24,14 @@ public class ZipArchiver {
     /**
      * Custom exception to throw if folder was not found
      */
-    class FolderNotFoundException extends Exception {
+    public class FolderNotFoundException extends Exception {
 
     }
 
     /**
      * Custom exception to throw in case of zip file problems
      */
-    class ZipException extends Exception {
+    public class ZipException extends Exception {
 
     }
 
@@ -41,20 +42,20 @@ public class ZipArchiver {
 
     /**
      * scans all archives in directory, unpacks only files that match the regex
-     * @throws FolderNotFoundException
-     * @throws FileNotFoundException
-     * @throws ZipException
+     * @throws FolderNotFoundException if such folder was not found
+     * @throws FileNotFoundException if such file was not found
+     * @throws ZipException if problems with zipping occured
      */
-    public void unpackRegex() throws FolderNotFoundException, FileNotFoundException, ZipException {
+    public void unpackRegex() throws FolderNotFoundException, ZipException, FileNotFoundException {
         File folder = new File(path);
-        try {
-            for (File fileEntry : folder.listFiles()) {
-
+        if (folder.listFiles() == null) {
+            throw new FolderNotFoundException();
+        }
+        for (File fileEntry : folder.listFiles()) {
+            try (BufferedInputStream fileInputStream = new BufferedInputStream(new FileInputStream(fileEntry));
+                 ZipInputStream zipInputStream = new ZipInputStream(fileInputStream)
+                 ) {
                 ZipFile zipFile = new ZipFile(fileEntry);
-
-                BufferedInputStream fileInputStream = new BufferedInputStream(new FileInputStream(fileEntry));
-
-                ZipInputStream zipInputStream = new ZipInputStream(fileInputStream);
 
                 ZipEntry zipEntry = zipInputStream.getNextEntry();
 
@@ -67,13 +68,10 @@ public class ZipArchiver {
                     }
                     zipEntry = zipInputStream.getNextEntry();
                 }
+            } catch (IOException e) {
+                throw new ZipException();
             }
-        } catch (NullPointerException e) {
-            throw new FolderNotFoundException();
-        } catch (IOException e) {
-            throw new ZipException();
         }
-
     }
 
     /**
@@ -82,16 +80,15 @@ public class ZipArchiver {
      * @param zipEntry file to unzip
      */
     private void writeEntry(ZipFile zipFile, ZipEntry zipEntry) {
-        try {
-            InputStream inputStream = zipFile.getInputStream(zipEntry);
-            FileOutputStream fileOutputStream = new FileOutputStream(zipEntry.getName());
+        try (InputStream inputStream = zipFile.getInputStream(zipEntry);
+             FileOutputStream fileOutputStream = new FileOutputStream(zipEntry.getName());
+            ){
+
             byte[] bytes = new byte[1024];
             int length;
             while ((length = inputStream.read(bytes)) >= 0) {
                 fileOutputStream.write(bytes, 0, length);
             }
-            inputStream.close();
-            fileOutputStream.close();
         } catch (IOException e) {
             System.out.println("Unable to unpack " + zipEntry.getName());
         }
@@ -107,12 +104,19 @@ public class ZipArchiver {
     }
 
     public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Invalid number of arguments, try again");
+            return;
+        }
+
         String path = args[0];
         String regex = args[1];
 
-        ZipArchiver zipArchiver = new ZipArchiver(path, regex);
         try {
+            ZipArchiver zipArchiver = new ZipArchiver(path, regex);
             zipArchiver.unpackRegex();
+        } catch (PatternSyntaxException e) {
+            System.out.println("Invalid regular expression");
         } catch (ZipException e) {
             System.out.println("Unable to access zip file");
         } catch (FileNotFoundException e) {
